ea134c350d32a5bdf2901e8bfea022d92f468f04
alsgur0720 <alsgur4520@gmail.com>
2022-07-17 16:56:29 +0900

    1st

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        <script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=s2hwv5i92t"></script>
       </Head>
 
       <main>
@@ -21,3 +23,4 @@ export default function Home() {
     </div>
   )
 }
+
